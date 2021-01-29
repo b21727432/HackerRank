3db52d4db6c30545569306f99d87332f5cf1cb86
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-01-29 22:16:53 +0300

    Add files via upload

---
+++ c++ makro example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#define PR(id) printf("The value of " #id " is”)
+int main(){
+	int i = 10;
+	PR(i);                                                    
+	return 0;
+}
+
