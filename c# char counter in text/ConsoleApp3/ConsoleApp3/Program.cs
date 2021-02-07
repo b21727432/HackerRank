948a618bba1886ca3459f38256fe4593f59e273b
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-02-08 00:23:15 +0300

    Add files via upload

---
+++ c# char counter in text/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C:\Users\grupofis\Desktop
+            Console.WriteLine("Dosyanin adresini giriniz :");
+            string adres = Console.ReadLine();
+            if (File.Exists(adres))
+            {
+                string text = File.ReadAllText(adres);
+                //Console.WriteLine(text);
+
+                var harfSozluk = new Dictionary<char, int>();
+                string yazma = "";
+                foreach(char seciliHarf in text)
+                {
+                    if (harfSozluk.ContainsKey(seciliHarf))
+                    {
+                        harfSozluk[seciliHarf]++;
+                    }
+                    else
+                    {
+                        harfSozluk.Add(seciliHarf, 1);
+                    }
+                }
+
+                foreach(char seciliHarf in harfSozluk.Keys)
+                {
+                    Console.Write(seciliHarf + "(" + harfSozluk[seciliHarf] + ") ");
+                    yazma += seciliHarf + "(" + harfSozluk[seciliHarf] + ") ";
+                    
+                }
+                Console.WriteLine("\nYazilicak Dosyanin adresini giriniz :");
+                string ciktiadres = Console.ReadLine();
+                File.WriteAllText(@ciktiadres, yazma);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ c# word counter in text/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dosya Dizinini Giriniz :");
+            string adres = Console.ReadLine();
+            adres = adres + "\\c.txt";
+            Console.WriteLine("Kac Cumle Ekleyeceksiniz? :");
+            int cumlesayisi = Convert.ToInt32(Console.ReadLine());
+
+            using (var sw = new StreamWriter(adres))
+            {
+                for (var i = 0; i < cumlesayisi; i++)
+                {
+                    Console.WriteLine("Cumleyi Giriniz :");
+                    string cumle = Console.ReadLine();
+                    sw.WriteLine(cumle);
+                }
+            }
+
+            string text = File.ReadAllText(adres);
+            Console.WriteLine(text);
+            Console.WriteLine("Aramak istediginiz kelimeyi giriniz :");
+
+
+            string aranan = Console.ReadLine();
+            string[] source = text.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' , '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == aranan.ToLowerInvariant()
+                             select word;
+             int wordCount = matchQuery.Count();
+            Console.WriteLine("{0} adet \"{1}\" kelimesi bulundu.", wordCount, aranan);
+        }
+    }
+}
