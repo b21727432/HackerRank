c14d1ab9ce7b67e60aedf883b468a427976ee155
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-05-31 11:59:30 +0300

    Add files via upload

---
+++ one queue to another queue.cpp
@@ -0,0 +1,20 @@
+#include <queue>
+
+
+int main(){
+	
+	std::queue<int> kareliqueue;
+	while (!verilenqueue.empty())
+  {
+  	kareliqueue.push(verilenqueue.front() ** 2);
+  	std::cout << ' ' << verilenqueue.front();
+    verilenqueue.pop();
+  }
+	
+	while (!kareliqueue.empty())
+  {
+  	std::cout << ' ' << kareliqueue.front();
+    kareliqueue.pop();
+  }
+	return 0;
+}
