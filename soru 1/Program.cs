64d069da8535440a0943a572b6be00a6fa3d4d43
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-11-17 10:42:56 +0300

    Add files via upload

---
+++ soru 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. sayiyi girin : ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. sayiyi girin : ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            int sayi1toplam = 0;
+            int sayi2toplam = 0;
+            for(int i = 1; i < sayi1; i++)
+            {
+                if(sayi1 % i == 0)
+                {
+                    sayi1toplam = sayi1toplam + i;
+                }
+            }
+            for(int i = 1; i < sayi2; i++)
+            {
+                if(sayi2 % i == 0)
+                {
+                    sayi2toplam = sayi2toplam + i;
+                }
+            }
+            if(sayi1toplam == sayi2 && sayi2toplam == sayi1)
+            {
+                Console.WriteLine("Bu sayilar arkadas sayilardir");
+            }
+            else
+            {
+                Console.WriteLine("Bu sayilar arkadas sayi degildir");
+            }
+        }
+    }
+}
