b7fa90b42b4f328b6040ee6c19cbb06ed4e3c1d6
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-11-17 10:48:39 +0300

    Add files via upload

---
+++ soru 6/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kisi sayisini giriniz:");
+            int kisi = Convert.ToInt32(Console.ReadLine());
+            int tokalasma = 0;
+            for(int i = kisi; i > 1; i--)
+            {
+                tokalasma += i - 1;
+            }
+            Console.WriteLine("Toplam Tokalasma Sayisi : " + tokalasma);
+        }
+    }
+}
