7d4b518e73639330bd9059af6e91b07ab89c82c2
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-11-17 10:50:29 +0300

    Add files via upload

---
+++ soru 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Sayilar Olusturuluyor...");
+            Random rnd = new Random();
+            int[] loto_sayilar = new int[6];
+            for(int i = 0; i < 6; i++)
+            {
+                loto_sayilar[i] = rnd.Next(1, 49);
+            }
+            Console.WriteLine("Random Sayilar:");
+            for(int i = 0; i < 6; i++)
+            {
+                Console.Write(loto_sayilar[i] + " ");
+            }
+        }
+    }
+}
