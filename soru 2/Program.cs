189a44a35dbd1ef7dad494cac2ab1b75926d3175
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-11-17 10:43:38 +0300

    Add files via upload

---
+++ soru 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A noktasi x koordinatini giriniz : ");
+            int ax = Convert.ToInt32(Console.ReadLine());
+            Console.Write("A noktasi y koordinatini giriniz : ");
+            int ay = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B noktasi x koordinatini giriniz : ");
+            int bx = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B noktasi y koordinatini giriniz : ");
+            int by = Convert.ToInt32(Console.ReadLine());
+            if(bx == ax)
+            {
+                Console.Write("Dogrunun Egimi Sonsuzdur.");
+            } else if(by == ay)
+            {
+                Console.Write("Dogrunun Egimi Sifirdir.");
+            }
+            else
+            {
+                double egim = (by - ay) / (bx - ax);
+                Console.Write("Dogrunun Egimi : " + egim);
+            }
+            
+        }
+    }
+}
