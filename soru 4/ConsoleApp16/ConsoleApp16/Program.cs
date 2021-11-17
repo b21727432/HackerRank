24abd79d2921021ff38559a2470eaa8d8a47d47a
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-11-17 10:44:55 +0300

    Add files via upload

---
+++ soru 4/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cekilicek Miktar Giriniz : ");
+            double miktar = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Vade Tarihi Giriniz : ");
+            double vade = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Kredi Turunu Giriniz : ");
+            string tur = Console.ReadLine();
+            double faiz, odeme = 0;
+            if(tur == "Konut Kredisi")
+            {
+                faiz = 1.08;
+                odeme = (miktar * faiz / 100) / (1.0 - (1.0 / (Math.Pow((1.0 + faiz/100), vade))));
+                Console.WriteLine("Aylik Odeme Miktari : " + odeme.ToString("0.00000") + " Toplam Odenecek Miktar : " + (odeme*vade).ToString("0.00000") );
+            } else if(tur == "Tasit Kredisi")
+            {
+                faiz = 1.25;
+                odeme = (miktar * faiz / 100) / (1.0 - (1.0 / (Math.Pow((1.0 + faiz / 100), vade))));
+                Console.WriteLine("Aylik Odeme Miktari : " + odeme.ToString("0.00000") + " Toplam Odenecek Miktar : " + (odeme * vade).ToString("0.00000"));
+            }
+            else
+            {
+                Console.WriteLine("Duzgun bir kredi turu girmediniz..");
+            }
+        }
+    }
+}
