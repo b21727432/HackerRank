f2bcf1b230478d4590bf39c6002389eb2358b29d
b21727432 <32540531+b21727432@users.noreply.github.com>
2021-11-17 10:45:20 +0300

    Add files via upload

---
+++ soru 5/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mukemmel sayi kontrolu yapilicak sayiyi giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int bolen_toplam = 0;
+            for(int i = 1; i < sayi; i++)
+            {
+                if(sayi % i == 0)
+                {
+                    bolen_toplam += i;
+                }
+            }
+            if(bolen_toplam == sayi)
+            {
+                Console.WriteLine("Girdiginiz Sayi Mukemmel Sayidir.");
+            }
+            else
+            {
+                Console.WriteLine("Girdiginiz Sayi Mukemmel Sayi Degildir.");
+            }
+        }
+    }
+}
